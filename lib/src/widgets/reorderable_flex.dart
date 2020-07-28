bee9f8804ffb393bc1c99cac2785b1c5b4419eff
nbothmer <n.bothmer@neusta.de>
2020-07-28 06:54:03 +0200

    _draggingFeedbacksize inital value 0,0

--- lib/src/widgets/reorderable_flex.dart
+++ lib/src/widgets/reorderable_flex.dart
@@ -232,7 +232,7 @@ class _ReorderableFlexContentState extends State<_ReorderableFlexContent>
   Widget _draggingWidget;
 
   // The last computed size of the feedback widget being dragged.
-  Size _draggingFeedbackSize = Size(0,0);
+  Size _draggingFeedbackSize = Size(0, 0);
 
   // The location that the dragging widget occupied before it started to drag.
   int _dragStartIndex = -1;
